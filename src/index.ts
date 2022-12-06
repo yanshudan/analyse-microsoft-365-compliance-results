aeb3d12f1acf199739b9ec77a0769f04ae101a9e
Qinzhou Xu <qinzhouxu@microsoft.com>
2022-12-06 15:54:40 +0800

    enable create report

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 import * as core from "@actions/core";
+import { AppComplianceAutomationToolForMicrosoft365 } from "@azure/arm-appcomplianceautomation";
+import { AzureCliCredential } from "@azure/identity";
 
-function start() {
+async function start() {
   try {
+    const cred = new AzureCliCredential();
+    const token = await cred.getToken("https://management.azure.com//.default");
+
+    const client = new AppComplianceAutomationToolForMicrosoft365(cred);
+
+    const params = {
+      properties: {
+        resources: [
+          {
+            resourceId: "/subscriptions/1a033ab8-736d-473f-9927-65241c799738/resourceGroups/TESTVM/providers/Microsoft.Compute/virtualMachines/smallvm",
+            tags: {}
+          }
+        ],
+        timeZone: "China Standard Time",
+        triggerTime: new Date("2022-12-05T18:00:00.000Z")
+      }
+    };
+
+    const options = {
+      requestOptions: {
+        customHeaders: {
+          "Authorization": `Bearer ${token.token}`,
+          "x-ms-aad-user-token": `Bearer ${token.token}`,
+          "Content-Type": "application/json"
+        }
+      }
+    };
+
     const reportName = core.getInput('report-name');
-    console.log(reportName);
+    const req = await client.report.beginCreateOrUpdate(reportName, params, options);
+    await req.pollUntilDone();
+
+    console.log(`Successfully created report ${reportName}`);
+
   } catch (error) {
     core.setFailed(error.message);
   }
